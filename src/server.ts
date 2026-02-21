22fa810b0470b90cdeb22dcffde0ca447fbab6f2
trmtanjil <trmtanjil02@gmail.com>
2026-02-21 23:16:05 +0600

    server run properly commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+const port = process.env.PORT || 5000; // The port your express server will be running on.
+
+
+
+ 
+const bootstrap = async () => {
+ try {
+   // Start the server
+   app.listen(port, () => {
+     console.log(`Server is running on http://localhost:${port}`);
+   });
+ } catch (error) {
+   console.error("Error starting the server:", error);
+ }
+  }
+
+bootstrap();
