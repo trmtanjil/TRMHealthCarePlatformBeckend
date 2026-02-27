450c05ac103defd76b2a6e5d0037b86b92f7c37c
trmtanjil <trmtanjil02@gmail.com>
2026-02-28 00:38:55 +0600

    cors add

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,12 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve(process.cwd(), `src/app/templates`))
 app.use("api/auth",toNodeHandler(auth))
 
+app.use(cors({
+  origin : [  "http://localhost:3000", "http://localhost:5000"],
+  credentials : true,
+  methods : ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders : ["Content-Type", "Authorization"]
+}))
 
 app.use("/api/auth", toNodeHandler(auth))
 // Enable URL-encoded form data parsing
