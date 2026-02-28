03deedbf226d8fd242e1e7b15c4bd3ee7cf8213d
trmtanjil <trmtanjil02@gmail.com>
2026-02-28 13:51:30 +0600

    Creating Function To upload File From Cloudinary

--- src/app/config/cloudinary.config.ts
+++ src/app/config/cloudinary.config.ts
@@ -1,4 +1,4 @@
-import {v2 as cloudinary} from "cloudinary"
+import {v2 as cloudinary, UploadApiResponse} from "cloudinary"
 import { envVars } from "./env"
 import AppError from "../errorHelpers/AppError";
 import status from "http-status";
@@ -9,6 +9,54 @@ cloudinary.config({
     api_secret:envVars.CLOUDINARY.API_SECRET
 })
 
+export const uploadFileToCloudinary = async (
+    buffer : Buffer,
+    fileName: string,
+) : Promise<UploadApiResponse> =>{
+
+    if(!buffer || !fileName) {
+        throw new AppError(status.BAD_REQUEST, "File buffer and file name are required for upload");
+    }
+
+    const extension = fileName.split(".").pop()?.toLocaleLowerCase();
+
+    const fileNameWithoutExtension = fileName
+        .split(".")
+        .slice(0, -1)
+        .join(".")
+        .toLowerCase()
+        .replace(/\s+/g, "-")
+        // eslint-disable-next-line no-useless-escape
+        .replace(/[^a-z0-9\-]/g, "");
+
+    const uniqueName =
+        Math.random().toString(36).substring(2) +
+        "-" +
+        Date.now() +
+        "-" +
+        fileNameWithoutExtension;
+
+    const folder = extension === "pdf" ? "pdfs" : "images";
+
+
+    return new Promise((resolve, reject) => {
+        cloudinary.uploader.upload_stream(
+            {
+                resource_type: "auto",
+                public_id: `trm-healthcare/${folder}/${uniqueName}`,
+                folder : `trm-healthcare/${folder}`,
+            },
+            (error, result) => {
+                if(error){
+                    return reject(new AppError(status.INTERNAL_SERVER_ERROR, "Failed to upload file to Cloudinary"));
+                }
+                resolve(result as UploadApiResponse);
+            }
+        ).end(buffer);
+    })
+
+
+}
 
 export const deleteFileFromCloudinary = async (url : string) => {
 
