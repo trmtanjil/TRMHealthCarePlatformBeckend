92131404b2fb91fc5c332690cc9bdc4e8cb090b1
trmtanjil <trmtanjil02@gmail.com>
2026-03-04 14:47:57 +0600

    updatedoctorSchedule service   create

--- src/app/module/doctorSchedule/doctorSchedule.controller.ts
+++ src/app/module/doctorSchedule/doctorSchedule.controller.ts
@@ -3,13 +3,12 @@ import status from "http-status";
  import { catchAsync } from "../../shared/catchAsync";
 import { sendResponse } from "../../shared/sendResponse";
 import { DoctorScheduleService } from "./doctorSchedule.service";
-import { ICreateDoctorSchedulePayload } from "./doctorSchedule.interface";
  
 const createMyDoctorSchedule = catchAsync( async (req : Request, res : Response) => {
     const payload = req.body;
     const user = req.user;
  
-    const doctorSchedule = await DoctorScheduleService.createMyDoctorSchedule(user as IRequestUser, payload as ICreateDoctorSchedulePayload);
+    const doctorSchedule = await DoctorScheduleService.createMyDoctorSchedule(user!  , payload  );
     sendResponse(res, {
         success: true,
         httpStatusCode: status.CREATED,
@@ -53,8 +52,10 @@ const getDoctorScheduleById = catchAsync(async (req: Request, res: Response) => 
 });
 
 const updateMyDoctorSchedule = catchAsync( async (req : Request, res : Response) => {
+    const payload = req.body;
+    const user = req.user;
      
-    const updatedDoctorSchedule = await DoctorScheduleService.updateMyDoctorSchedule( );
+    const updatedDoctorSchedule = await DoctorScheduleService.updateMyDoctorSchedule( user!, payload );
     sendResponse(res, {
         success: true,
         httpStatusCode: status.OK,  
--- src/app/module/doctorSchedule/doctorSchedule.service.ts
+++ src/app/module/doctorSchedule/doctorSchedule.service.ts
@@ -1,6 +1,6 @@
 import { IRequestUser } from "../../interfaces/requestUser.interface"
 import { prisma } from "../../lib/prisma"
-import { ICreateDoctorSchedulePayload } from "./doctorSchedule.interface"
+import { ICreateDoctorSchedulePayload, IUpdateDoctorSchedulePayload } from "./doctorSchedule.interface"
 
 const createMyDoctorSchedule = async (user:IRequestUser, payload:ICreateDoctorSchedulePayload ) => { 
     const doctorData = await prisma.doctor.findUniqueOrThrow({
@@ -31,7 +31,37 @@ const getDoctorScheduleById = async ( ) => {
 }
 
 
-const updateMyDoctorSchedule = async ( ) => {
+const updateMyDoctorSchedule = async ( user:IRequestUser,payload:IUpdateDoctorSchedulePayload) => {
+    const doctorData = await prisma.doctor.findUniqueOrThrow({
+        where: {
+            email: user.email
+        },
+    })
+    const deleteIds = payload.scheduleIds.filter((schedule) => schedule.shouldDelete).map((schedule) => schedule.id)
+     
+    const createIds = payload.scheduleIds.filter((schedule) => !schedule.shouldDelete).map((schedule) => schedule.id)
+
+    const result =  await prisma.$transaction(async (tx) => {
+
+        await tx.doctorSchedules.deleteMany({
+            where: {
+                doctorId: doctorData.id,
+                scheduleId: {
+                    in: deleteIds
+                }
+            }
+        });
+        const doctorScheduleData = createIds.map((scheduleId) =>  ({
+            doctorId : doctorData.id,
+            scheduleId
+        }))
+        const result = await tx.doctorSchedules.createMany({
+            data: doctorScheduleData
+        })
+        return result
+
+    })
+    return result
 }
 
 const deleteMyDoctorSchedule = async ( ) => {
