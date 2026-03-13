0421272b2a6467374d03acf120b9a2647f8486db
trmtanjil <trmtanjil02@gmail.com>
2026-03-14 04:54:13 +0600

    super admin seeding work succesfully login :

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,6 +4,9 @@ import { authRoute } from "../module/auth/auth.route";
 import { userRoutes } from "../module/user/user.route";
 import { doctorRoutes } from "../module/doctor/doctor.route";
 import { AdminRoutes } from "../module/admin/admin.route";
+import { DoctorScheduleRoutes } from "../module/doctorSchedule/doctorSchedule.route";
+import { AppointmentRoutes } from "../module/appointment/appointment.route";
+import { scheduleRoutes } from "../module/schedule/schedule.route";
 
 const router = Router()
  router.use("/auth", authRoute)  
@@ -12,5 +15,10 @@ router.use("/specialties",SpecialtyRoutes)
 router.use("/users", userRoutes)
 router.use("/doctors", doctorRoutes)
 router.use("/admins", AdminRoutes)
+
+router.use("/schedules", scheduleRoutes)
+router.use("/doctor-schedules", DoctorScheduleRoutes)
+router.use("/appointments", AppointmentRoutes)
+
 
 export const IndexRoutes = router
