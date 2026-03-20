ae9585c6f0462e233eef577243efb2f69a3d8a58
trmtanjil <trmtanjil02@gmail.com>
2026-03-20 23:41:24 +0600

    get all patient

---
+++ src/app/module/patient/patient.controller.ts
@@ -0,0 +1,11 @@
+const getAllDoctorSchedules = catchAsync(async (req: Request, res: Response) => {
+    const query = req.query;
+     const result  = await DoctorScheduleService.getAllDoctorSchedules(query as IquearyParams);
+    sendResponse(res, {
+        success: true,
+        httpStatusCode: status.OK,
+        message: 'All doctor schedules retrieved successfully',
+        data: result.data,
+        meta: result.meta
+    });
+});
