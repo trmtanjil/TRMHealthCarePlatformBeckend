524bd281e3a72e478376d02673a7661c8aeff748
trmtanjil <trmtanjil02@gmail.com>
2026-03-23 20:27:19 +0600

    Update My Profile Route For Patient Role Part-2

--- src/app/module/patient/patient.controller.ts
+++ src/app/module/patient/patient.controller.ts
@@ -9,3 +9,14 @@ const getAllDoctorSchedules = catchAsync(async (req: Request, res: Response) => 
         meta: result.meta
     });
 });
+const getAllDoctorSchedules = catchAsync(async (req: Request, res: Response) => {
+    const query = req.query;
+     const result  = await DoctorScheduleService.getAllDoctorSchedules(query as IquearyParams);
+    sendResponse(res, {
+        success: true,
+        httpStatusCode: status.OK,
+        message: 'All doctor schedules retrieved successfully',
+        data: result.data,
+        meta: result.meta
+    });
+});
