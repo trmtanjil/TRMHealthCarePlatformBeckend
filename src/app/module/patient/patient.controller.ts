512da30e1212fa5f512dd6bb9721a11e80c6ff30
trmtanjil <trmtanjil02@gmail.com>
2026-03-23 20:27:32 +0600

    get all patient

--- src/app/module/patient/patient.controller.ts
+++ src/app/module/patient/patient.controller.ts
@@ -9,14 +9,3 @@ const getAllDoctorSchedules = catchAsync(async (req: Request, res: Response) => 
         meta: result.meta
     });
 });
-const getAllDoctorSchedules = catchAsync(async (req: Request, res: Response) => {
-    const query = req.query;
-     const result  = await DoctorScheduleService.getAllDoctorSchedules(query as IquearyParams);
-    sendResponse(res, {
-        success: true,
-        httpStatusCode: status.OK,
-        message: 'All doctor schedules retrieved successfully',
-        data: result.data,
-        meta: result.meta
-    });
-});
